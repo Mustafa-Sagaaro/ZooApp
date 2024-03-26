94cabb368b121c60473ab324cfc940d314cd2563
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2024-03-26 14:45:38 +0100

    Funktion zum zurückgehen hinzugefügt

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -1,16 +1,30 @@
 import { useParams } from 'react-router-dom';
-import newsData from './Newsdata.jsx'; 
+import { useNavigate } from 'react-router-dom';
+import newsData from './Newsdata.jsx';
 import "../styles/Newsdetails.css"
+
 const NewsDetails = () => {
   const { newsId } = useParams();
   const newsItem = newsData.find(item => item.id === parseInt(newsId, 10));
+  const navigate = useNavigate();
 
+  const goBack = () => {
+    navigate(-1); // Navigiert einen Schritt zurück im Browser-Verlauf
+  };
   if (!newsItem) {
     return <div>News item not found.</div>;
   }
 
   return (
+
     <article className="blog-post">
+      <img
+        src="https://cdn.iconscout.com/icon/free/png-256/free-back-arrow-1767507-1502574.png" // Pfad zu deinem Zurück-Icon
+        alt="Go back"
+
+        onClick={goBack}
+        style={{ cursor: 'pointer', width: '50px'}} // Cursor-Stil hinzufügen, um Klickbarkeit anzuzeigen
+      />
       <header className="blog-post-header">
         <h1 className="blog-post-title">{newsItem.title}</h1>
         <p className="blog-post-date">{newsItem.date}</p>
